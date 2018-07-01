2baad3d1b27f4b858eb4ed36bfc5334df906f156
ramvignesh30 <ramvignesh30@gmail.com>
2018-07-01 03:25:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+
+function generateCars(numberOfCars) {
+    const cars = [];
+
+    const carMakes = ['Honda', 'BMW','Fiat','Skoda','Volvo'];
+    const carColors = ['lightgrey', 'lightcyan','lightyellow','lightgreen','lightcoral','lightpink'];
+    
+    for (let i = 0; i < numberOfCars; i++) {
+        const car = {};
+        const randomMakeIndex = randomIntFromInterval(0, carMakes.length - 1);
+        const randomColorIndex = randomIntFromInterval(0, carColors.length - 1);
+
+        car.make = carMakes[randomMakeIndex];
+        car.color = carColors[randomColorIndex];
+        car.speed = randomIntFromInterval(0, 100);
+
+        cars.push(car);
+    }
+
+    return cars;
+}
